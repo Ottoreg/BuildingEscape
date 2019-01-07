731280ef8946155f4f91b9bfe3deff91c936e419
Ottoreg <benjamingerotto@gmail.com>
2019-01-07 12:02:54 +0100

    create RotateDoor

---
+++ Source/BuildingEscape/RotateDoor.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RotateDoor.h"
+
+
+// Sets default values for this component's properties
+URotateDoor::URotateDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void URotateDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FRotator rotation(0, 0, 0); // pitch YAW roll
+
+	GetOwner()->SetActorRotation(rotation);
+	// ...
+	
+}
+
+
+// Called every frame
+void URotateDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
